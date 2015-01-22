a454bee4a16da284d8d3434f7689cdb23a5d10ad
unknown <tacketra@537-0501.CEAS1.uc.edu>
2015-01-22 17:31:58 -0500

    first commit

---
+++ EECE3093SS15/src/tracing/views/RequirementsIndicesView.java
@@ -0,0 +1,163 @@
+package tracing.views;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.events.ShellListener;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.part.ViewPart;
+
+public class RequirementsIndicesView extends ViewPart implements ISelectionProvider{
+	
+	private void showMessage(){
+		//final Display display = new Display();
+		Shell shell = new Shell();
+	    //shell.setLayout(new FillLayout());
+
+	   shell.addShellListener(new ShellListener() {
+
+	      public void shellActivated(ShellEvent event) {
+	        System.out.println("activate");
+	      }
+
+	      public void shellClosed(ShellEvent arg0) {
+	        System.out.println("close");
+	      }
+
+	      public void shellDeactivated(ShellEvent arg0) {
+	      }
+
+	      public void shellDeiconified(ShellEvent arg0) {
+	      }
+
+	      public void shellIconified(ShellEvent arg0) {
+	      }
+	    });
+	    
+		
+	    /*
+	    shell.open();
+	    // Set up the event loop.
+	    while (!shell.isDisposed()) {
+	      if (!display.readAndDispatch()) {
+	        // If no more entries in event queue
+	        display.sleep();
+	      }
+	    }
+	    display.dispose();
+	    
+	    */
+	   /*MessageDialog(Shell parentShell, String dialogTitle, Image dialogTitleImage, 
+	    * String dialogMessage, int dialogImageType, String[] dialogButtonLabels,
+	    *  int defaultIndex)
+	    */
+	    String[] buttonArray = new String[]{"Browse1", "Browse2", "Browse3"};
+	    MessageDialog md = new MessageDialog(shell, "feature 1", null, 
+	    		"contentDescription", 0, null, 0);
+
+		md.openInformation(shell,
+				"Testhello",
+				"Hello, Eclipse world");
+
+		
+	}
+	
+	@Override
+	public void addSelectionChangedListener(ISelectionChangedListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ISelection getSelection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeSelectionChangedListener(
+			ISelectionChangedListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setSelection(ISelection selection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		
+		showMessage();
+		
+		//Set layout forum of parent composite
+		parent.setLayout(new FormLayout());
+		
+		FormData formdata = new FormData();
+		formdata.top=new FormAttachment(0,5);
+		formdata.left = new FormAttachment(0,10);
+		formdata.right = new FormAttachment(0,200);
+		
+		//Create title label
+		Label titleLabel = new Label(parent,SWT.SINGLE);
+		titleLabel.setText("Requirements Indices:");
+		titleLabel.setLayoutData(formdata);
+		
+		//Create text area
+		Text indicesText = new Text(parent,SWT.MULTI|SWT.V_SCROLL|SWT.READ_ONLY);
+		indicesText.setText("This is a sample result.");
+		formdata = new FormData();
+		formdata.top = new FormAttachment(titleLabel,10);
+		formdata.bottom = new FormAttachment(titleLabel,230);
+		formdata.left = new FormAttachment(0,10);
+		formdata.right = new FormAttachment(0,800);
+		indicesText.setLayoutData(formdata);
+		
+		
+		Button manageButton = new Button(parent,SWT.PUSH);
+		manageButton.setText("Manage...");
+		formdata = new FormData();
+		formdata.top = new FormAttachment(indicesText,10);
+		formdata.left = new FormAttachment(0,730);
+		manageButton.setLayoutData(formdata);
+		
+		manageButton.addSelectionListener(new SelectionListener(){
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				showMessage();
+				
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ EECE3093SS15/src/tracing/views/RequirementsView.java
@@ -0,0 +1,155 @@
+package tracing.views;
+
+
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.part.*;
+import org.eclipse.jface.viewers.*;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.jface.action.*;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.*;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.SWT;
+
+
+/**
+ * This sample class demonstrates how to plug-in a new
+ * workbench view. The view shows data obtained from the
+ * model. The sample creates a dummy model on the fly,
+ * but a real implementation would connect to the model
+ * available either in this or another plug-in (e.g. the workspace).
+ * The view is connected to the model using a content provider.
+ * <p>
+ * The view uses a label provider to define how model
+ * objects should be presented in the view. Each
+ * view can present the same model objects using
+ * different labels and icons, if needed. Alternatively,
+ * a single label provider can be shared between views
+ * in order to ensure that objects of the same type are
+ * presented in the same way everywhere.
+ * <p>
+ */
+
+public class RequirementsView extends ViewPart implements ISelectionProvider{
+	
+	private ISelection selection;
+	private ComboViewer comboViewer;
+	
+	/**
+	 * The ID of the view as specified by the extension.
+	 */
+	public static final String ID = "tracing.views.RequirementsView";
+	
+	/**
+	 * The constructor.
+	 */
+	public RequirementsView() {
+	}
+
+	/**
+	 * This is a callback that will allow us
+	 * to create the viewer and initialize it.
+	 */
+	@Override
+	public void createPartControl(Composite parent) {
+		//Set layout forum of parent composite
+		parent.setLayout(new FormLayout());
+		
+		//Create a drop box
+		comboViewer = new ComboViewer(parent,SWT.NONE|SWT.DROP_DOWN);
+		Combo combo = comboViewer.getCombo();
+		combo.add("Choose Use Case");
+		combo.add("UC1");
+		
+		combo.select(0);
+		
+		//Set combo position
+		FormData formdata = new FormData();
+		formdata.top=new FormAttachment(0,5);
+		formdata.left = new FormAttachment(0,10);
+		formdata.right = new FormAttachment(0,290);
+		combo.setLayoutData(formdata);
+		
+		//Set text position
+		Text text = new Text(parent,SWT.MULTI|SWT.V_SCROLL|SWT.READ_ONLY);
+		formdata = new FormData();
+		formdata.top=new FormAttachment(combo,10);
+		formdata.bottom = new FormAttachment(combo,600);
+		formdata.left = new FormAttachment(0,5);
+		formdata.right = new FormAttachment(0,355);
+		text.setLayoutData(formdata);
+		//set text content
+		text.setText("Indexing time of X requirement(s) is: Y seconds.");
+		
+		combo.addSelectionListener(new SelectionListener(){
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if(combo.getSelectionIndex()==0)
+					text.setText("Indexing time of X requirement(s) is: Y seconds.");
+				else if(combo.getSelectionIndex()==1)
+					text.setText("This is a sample.");
+				else
+					text.setText("");
+				
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		comboViewer.addSelectionChangedListener(new ISelectionChangedListener(){
+
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				ISelection comboSelection = event.getSelection();
+				setSelection(comboSelection);
+			}
+			
+		});
+		
+	}
+	
+	@Override
+	public void setSelection(ISelection selection) {
+		this.selection = selection;
+		SelectionChangedEvent event = new SelectionChangedEvent(comboViewer,selection);
+	}
+
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	@Override
+	public void setFocus() {
+	}
+
+	@Override
+	public void addSelectionChangedListener(ISelectionChangedListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ISelection getSelection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeSelectionChangedListener(
+			ISelectionChangedListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+}
